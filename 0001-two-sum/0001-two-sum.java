dc3e0bca72d3af5788a0aee4aef82065da7d0718
Meet21-hub <khandelwalmeet646@gmail.com>
2026-03-29 12:44:50 +0530

    Time: 2 ms (99.26%), Space: 47.2 MB (50.06%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,15 +1,14 @@
-//Brute Force Approach
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        for(int i=0; i < nums.length; i++) {
-            for(int j=i + 1; j < nums.length; j++) {
-                if (nums[j] == target - nums[i]) {
-                    // If pair is found
-                    return new int[] { i , j };
-                }
+        Map<Integer , Integer> map = new HashMap<>();
+        for(int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if(map.containsKey(complement)) {
+                return new int[] { map.get(complement) , i};
             }
+            map.put( nums[i] , i);
         }
-        // If no pair is found then
+        //If not found then 
         return new int[] {};
     }
 }
