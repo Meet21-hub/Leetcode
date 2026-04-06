73523e1915927f299d2a19f1b6a1b97414d8d1a9
Meet21-hub <khandelwalmeet646@gmail.com>
2026-04-06 20:05:08 +0530

    Time: 1 ms (99.84%), Space: 47.7 MB (81.66%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,14 +1,15 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int j =0;
+        int index = 0;
 
-        for( int i = 0; i < nums.length; i++) {
-            if( nums[i] != 0 ) {
-                int temp = nums[i];
-                nums[i] = nums[j];
-                nums[j] = temp;
-                j++;
+        for( int num : nums) {
+            if( num != 0 ) {
+                nums[index++] = num;
             }
         }
+
+        while ( index < nums.length) {
+                nums[index++] = 0;
+            }
     }
 }
